eb68bf976c10bed6b5134d3ad98a6b55c60e7637
Danilo Agrizzi <103227234+daniloagrizzi@users.noreply.github.com>
2023-11-19 22:58:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,13 +38,15 @@ function logar() {
   var email = document.getElementById('email').value;
   var senha = document.getElementById('senha').value;
 
-  if (email == "fornecedor@fortes.com.br") {
+   if (email == "fornecedor@fortes.com.br") {
     location.href = "home.html";
   }
   else if (email == "cliente@fortes.com.br") {
   location.href = "funcVerifc.html";
   }
-  
+   else {
+    alert('Usuario ou senha incorretos');
+  };
 
 };
 
